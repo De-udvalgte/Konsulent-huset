3c54cf9549061fc8972f7b616abeb23d25ad7f10
MieThorberg <cph-mt373@cphbusiness.dk>
2023-09-06 12:22:22 +0200

    made some changes to products

--- view/products.php
+++ view/products.php
@@ -11,7 +11,28 @@ $result = $product->read();
 <?php include 'components/header.php'; ?>
 <h1>Products</h1>
 
-<?php
-print_r($result);
-?>
+<table class="table">
+
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Price</th>
+    </tr>
+
+    <?php foreach (json_decode($result, true) as $product) { ?>
+
+        <tr>
+            <td><?php echo htmlspecialchars($product["productId"]) ?></td>
+            <td><?php echo htmlspecialchars($product["productName"]) ?></td>
+            <td><?php echo htmlspecialchars($product["productTitle"]) ?></td>
+            <td><?php echo htmlspecialchars($product["productDesc"]) ?></td>
+            <td><?php echo htmlspecialchars($product["price"]) ?></td>
+        </tr>
+    <?php
+    };
+    ?>
+</table>
+
 <?php include 'components/footer.php'; ?>

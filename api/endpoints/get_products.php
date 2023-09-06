31f12993d3c997347ec1e73bbb1665ecfddec905
PeterNedergaard <78910318+PeterNedergaard@users.noreply.github.com>
2023-09-06 13:28:16 +0200

    create get-products api

---
+++ api/endpoints/get_products.php
@@ -0,0 +1,50 @@
+<?php
+
+require('api/config/database.php');
+require('api/objects/product.php');
+
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+
+// instantiate product object
+$product = new Product($db);
+
+$stmt = $product->read();
+
+if ($stmt->rowCount() > 0) {
+    $products_arr = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+
+        $product_item = array(
+            "productId" => $productId,
+            "productName" => $productName,
+            "productDesc" => $productDesc,
+            "productTitle" => $productTitle,
+            "price" => $price
+        );
+
+        array_push($products_arr, $product_item);
+    }
+
+    // set response code - 200 OK
+    http_response_code(200);
+
+    // show products data in json format
+    echo json_encode($products_arr);
+} else {
+
+    // set response code - 404 Not found
+    http_response_code(404);
+
+    // tell the user no products found
+    echo json_encode(
+        array("message" => "No products found.")
+    );
+}
+
--- routes.php
+++ routes.php
@@ -4,5 +4,6 @@ require_once __DIR__.'/router.php';
 
 get('/konsulent-huset', 'view/index.php');
 get('/konsulent-huset/products', 'view/products.php');
+get('/konsulent-huset/api/products', 'api/endpoints/get_products.php');
 
 ?>

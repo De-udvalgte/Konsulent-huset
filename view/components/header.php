24829997120eebe2e46e2d855efee4738c288500
JonasRex <78900583+JonasRex@users.noreply.github.com>
2023-09-06 19:10:07 +0200

    Added simple Navbar + Styling

--- view/components/header.php
+++ view/components/header.php
@@ -4,8 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Konsulent-Huset</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./view/css/custom.css">
 </head>
-
 <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Konsulent-Huset</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="/konsulent-huset">Home</a>
+                    <a class="nav-item nav-link" href="/konsulent-huset/products">Products</a>
+                    <a class="nav-item nav-link" href="/konsulent-huset/create_product">Create Product</a>
+                    
+                    <!--
+                    <a class="nav-item nav-link" href="#">Logout</a>
+                    <a class="nav-item nav-link" href="#">Login</a>
+                    <a class="nav-item nav-link" href="#">Sign Up</a>
+                    -->
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
--- view/products.php
+++ view/products.php
@@ -1,33 +1,47 @@
-<?php
+<?php $result = file_get_contents('http://localhost/konsulent-huset/api/products'); ?>
 
-$result = file_get_contents('http://localhost/konsulent-huset/api/products');
-
-?>
 <?php include 'components/header.php'; ?>
-<h1>Products</h1>
+<main role="main" class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Products</h1>
 
-<table class="table">
+            <table class="table">
 
-    <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Price</th>
-    </tr>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                </tr>
 
-    <?php foreach (json_decode($result, true) as $product) { ?>
+                <?php foreach (json_decode($result, true) as $product) { ?>
 
-        <tr>
-            <td><?php echo htmlspecialchars($product["productId"]) ?></td>
-            <td><?php echo htmlspecialchars($product["productName"]) ?></td>
-            <td><?php echo htmlspecialchars($product["productTitle"]) ?></td>
-            <td><?php echo htmlspecialchars($product["productDesc"]) ?></td>
-            <td><?php echo htmlspecialchars($product["price"]) ?></td>
-        </tr>
-    <?php
-    };
-    ?>
-</table>
+                    <tr>
+                        <td>
+                            <?php echo htmlspecialchars($product["productId"]) ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($product["productName"]) ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($product["productTitle"]) ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($product["productDesc"]) ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($product["price"]) ?>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ;
+                ?>
+            </table>
+        </div>
+    </div>
+</main>
 
 <?php include 'components/footer.php'; ?>

6eeba99724f27db92590d253a942d2648b947041
cph-mm620 <70536109+cph-mm620@users.noreply.github.com>
2023-09-11 16:39:25 +0200

    update on navbar
    logo nu istedet for "konsulenthuset" + lidt ændringer i css

--- view/components/header.php
+++ view/components/header.php
@@ -19,7 +19,11 @@ if (!isset($_SESSION["userId"])) {
 <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Konsulent-Huset</a>
+            <div class="logo">
+            <a href="#" title="logo"></a>
+                    <img src="images/KHLOGO.png" alt="site logo" class="img-responsive">
+                </a>
+            </div>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>

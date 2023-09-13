fb634ca827ba418b5c8d54529400e8dcc33e9218
Louise-E <cph-le115@cphbusiness.dk>
2023-09-13 12:55:08 +0200

    Update not_found_error.php

--- view/errors/not_found_error.php
+++ view/errors/not_found_error.php
@@ -1,5 +1,9 @@
  <?php include 'view/components/header.php';?>
- <h1>Page Not Found</h1>
- <h5>Hmm... We could not find this page.</h5>
+ <div class="mt-4 p-5 bg-info text-muted rounded">
+  <div class="container">
+ <h1 style="text-align: center;">Page Not Found</h1>
+ <h5 style="text-align: center;">Hmm... We could not find this page.</h5>
+ </div>
+ </div>
  <?php include 'view/components/footer.php';?>
 
